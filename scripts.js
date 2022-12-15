dafb7be8812d993c9211c42a9e8c7e5cf70c3f59
AIT-ABBOU Samir <81538194+samir-aitabbou@users.noreply.github.com>
2022-12-15 02:04:33 +0100

    9

--- scripts.js
+++ scripts.js
@@ -24,30 +24,6 @@ setInterval(() => {
 
 
 
-//////////////////////////////////////////////////
-
-//********** Loads the MobileNet model and warms it up so ready for use (pre-entrained) *************//
- 
-async function loadMobileNetFeatureModel() {
-  const URL = 
-    'https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v3_small_100_224/feature_vector/5/default/1';
-  
-  mobilenet = await tf.loadGraphModel(URL, {fromTFHub: true});
-  STATUS.innerText = 'MobileNet v3 loaded successfully!';
-  
-  // Warm up the model by passing zeros through it once.
-  tf.tidy(function () {
-    let answer = mobilenet.predict(tf.zeros([1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3]));
-    console.log(answer.shape);
-  });
-}
-
-// Call the function immediately to start loading.
-loadMobileNetFeatureModel();
-
-
-
-
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -73,6 +49,28 @@ let predict = false;
 ENABLE_CAM_BUTTON.addEventListener('click', enableCam);
 TRAIN_BUTTON.addEventListener('click', trainAndPredict);
 RESET_BUTTON.addEventListener('click', reset);
+
+
+
+//********** Loads the MobileNet model and warms it up so ready for use (pre-entrained) *************//
+ 
+async function loadMobileNetFeatureModel() {
+  const URL = 
+    'https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v3_small_100_224/feature_vector/5/default/1';
+  
+  mobilenet = await tf.loadGraphModel(URL, {fromTFHub: true});
+  STATUS.innerText = 'MobileNet v3 loaded successfully!';
+  
+  // Warm up the model by passing zeros through it once.
+  tf.tidy(function () {
+    let answer = mobilenet.predict(tf.zeros([1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3]));
+    console.log(answer.shape);
+  });
+}
+
+// Call the function immediately to start loading.
+loadMobileNetFeatureModel();
+
 
 
 
